ec4563ee6587e0871aef359ccfa62934bbdcb796
NathanMach <peanuttypenpen1@yahoo.com>
2020-05-14 17:25:34 -0600

    nothing

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WhiteCard from './components/WhiteCard'
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WhiteCard />
+    </div>
+  );
+}
+
+export default App;
