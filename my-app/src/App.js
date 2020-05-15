9bab4f5b07f239ee44989348a9c8bb9321cd48e6
NathanMach <peanuttypenpen1@yahoo.com>
2020-05-14 21:46:10 -0600

    ben did this

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
+import React, {Component} from 'react';
 import WhiteCard from './components/WhiteCard'
-
 import './App.css';
+import Data from './components/Data'
 
-function App() {
-  return (
-    <div className="App">
-      <WhiteCard />
-    </div>
-  );
-}
+export default class App extends Component{
+  constructor(){
+    super()
+    this.state={
+      person: Data
+    }
+  }
 
-export default App;
+
+
+  render(){
+    return(
+      <div className='App'>
+        <header className='header'>
+          <h1>Home</h1>        
+        </header>
+        <div className='white'>
+          <div className='counter'>1/25</div>
+          <div className='info'>
+          <h3>First name</h3>
+          <br></br>
+          <p>From:</p>
+          <p>Job Title:</p>
+          <p>Employer:</p>
+          <br></br>
+          <h4>Favorite Movies:</h4>
+          <ol>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ol>
+          </div>
+        </div>
+        <div class='mother'>
+          <button className='button'>Previous</button>
+        <div>
+          <button className='butten'>Edit</button>
+          <button className='butten'>Delete</button>
+          <button className='butten'>New</button>
+        </div>
+        <button className='button'>Next</button>
+        </div>
+      </div>
+    )
+
+  }
+} 
+
+  
--- my-app/src/components/WhiteCard.js
+++ my-app/src/components/WhiteCard.js
@@ -1,33 +1,10 @@
 import React from 'react'
 
-export default class WhiteCard extends React.Component{
-    constructor(){
-     super()
-     this.state = {
-         name: 'Marion Santori',
-         from: 'Liverpool, United Kingdom',
-         jobTitle: 'Research Associate',
-         employer: 'Skinix',
-         favoriteMovies: ['Fathom', 'Strange Magic', 'Martian Child']
-        }
-    }
-    
-    
-    render(){
-        return <div>
-             <h1>Person no. {props.person.id}</h1>
-            <p>{props.person.firstName} {props.person.lastName}</p>
-            <span>name: </span>
-            <p>{props.person.name}</p>
-            <span>from:</span>
-            <p>{props.person.person}</p>
-            <span>Job Title:</span>
-            <p>{props.person.jobTitle}</p>
-            <span>employer:</span>
-            <p>{props.person.employer}</p>
-            <span>Favorite Movies:</span>
-            <p>{props.person.favoriteMovies}</p>
-        </div>
-    }
 
+function WhiteCard(){
+    return <div>
+        
+    </div>     
 }
+    
+
